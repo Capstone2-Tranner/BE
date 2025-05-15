35cc97b7497a54932920c0d23229923a6fec3a6d
choeunbin03 <choeunbiin03@naver.com>
2025-05-15 23:58:59 +0900

    [FEAT] external-api-proxy 구현 커밋 안 된 파일 다시 업로드

---
+++ external-api-proxy/src/main/java/com/tranner/external_api_proxy/ExternalApiProxyApplication.java
@@ -0,0 +1,13 @@
+package com.tranner.external_api_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalApiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalApiProxyApplication.class, args);
+	}
+
+}
