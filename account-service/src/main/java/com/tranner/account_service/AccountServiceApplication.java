ca759aa9d674bc50351562c716e7444eded00ce4
choeunbin03 <choeunbiin03@naver.com>
2025-05-18 17:07:52 +0900

    [FIX] account-service Eureka Client 등록  - application.yml 파일에 eureka client 설정 추가  - Application.java에 @DiscoveryClient 추가

--- account-service/src/main/java/com/tranner/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/com/tranner/account_service/AccountServiceApplication.java
@@ -2,9 +2,11 @@ package com.tranner.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.tranner.account_service")
 public class AccountServiceApplication {
 
