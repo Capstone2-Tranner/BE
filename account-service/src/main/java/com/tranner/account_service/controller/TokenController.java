9812b462440e62f64d4fedbe5e9d4dc8891ed9a1
choeunbin03 <choeunbiin03@naver.com>
2025-05-31 13:58:03 +0900

    [FIX] oauth2 로그인 로그 추가

--- account-service/src/main/java/com/tranner/account_service/controller/TokenController.java
+++ account-service/src/main/java/com/tranner/account_service/controller/TokenController.java
@@ -11,6 +11,7 @@ public class TokenController {
     @GetMapping("/login/success")
     public String serveTokenPage(@RequestParam("accessToken") String accessToken, Model model) {
         model.addAttribute("token", accessToken);
+        System.out.println("tokenController 진입");
         return "token"; // templates/token.html 렌더링
     }
 
